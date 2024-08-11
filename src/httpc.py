e3c1539827efa0ef276a7bcf1098532b0277f3a2
cipavv <lokidark56@gmail.com>
2024-08-11 09:14:25 -0600

    Update httpc.py

--- src/httpc.py
+++ src/httpc.py
@@ -14,16 +14,19 @@ def get_roblox_headers(user_agent = None, csrf_token = None, content_type = None
     Returns a dict of headers for Roblox requests
     """
     req_headers = {
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/json;charset=utf-8",
-        "Origin": "https://www.roblox.com",
-        "Referer": "https://www.roblox.com/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        "Te": "trailers"
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
+    'content-type': 'application/json',
+    'origin': 'https://www.roblox.com',
+    'priority': 'u=1, i',
+    'referer': 'https://www.roblox.com/',
+    'sec-ch-ua': '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
     }
 
     if user_agent is not None:
