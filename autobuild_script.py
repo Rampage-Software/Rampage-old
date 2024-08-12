2ae38309e145f14328f531c1f57a1b080e7a87fe
3rr0r <92172873+Its3rr0rsWRLD@users.noreply.github.com>
2024-08-11 20:03:24 -0500

    Revert "Add TLS Client DLL"
    This reverts commit 3bf4ea03d5cbe34af80b4b8bd5faf31ea16a2a4d.

--- autobuild_script.py
+++ autobuild_script.py
@@ -2,13 +2,26 @@ import os
 import subprocess
 import click
 import httpx
+import sys
+import venv
 
 # Paths
 base_dir = os.path.dirname(__file__)
 version_file_path = os.path.join(base_dir, "./src/data/version.py")
 exe_file_path = os.path.join(base_dir, "./dist/Privatools.exe")
-# Updated path for tls-client-64.dll
-tls_dll_path = os.path.join(base_dir, "tls_client/dependencies/tls-client-64.dll")
+venv_dir = os.path.join(base_dir, ".venv")
+tls_dll_path = os.path.join(venv_dir, "Lib/site-packages/tls_client/dependencies/tls-client-64.dll")
+
+# Set up virtual environment
+if not os.path.exists(venv_dir):
+    click.secho("Setting up virtual environment...", fg='yellow')
+    venv.create(venv_dir, with_pip=True)
+    subprocess.check_call([os.path.join(venv_dir, 'Scripts', 'python'), '-m', 'pip', 'install', '--upgrade', 'pip'])
+    subprocess.check_call([os.path.join(venv_dir, 'Scripts', 'python'), '-m', 'pip', 'install', 'click', 'httpx', 'pyinstaller', 'pyarmor'])
+
+# Activate virtual environment
+activate_script = os.path.join(venv_dir, 'Scripts', 'activate')
+exec(open(activate_script).read(), dict(__file__=activate_script))
 
 # URL for version info
 req_url = "https://raw.githubusercontent.com/Its3rr0rsWRLD/RAMPAGE/main/version.txt"
